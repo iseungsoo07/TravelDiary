021300ba23f252e855ad4bdf065be0762d1316d3
iseungsoo07 <iseungsoo07@gmail.com>
2023-11-23 17:05:00 +0900

    fix : SdkClientException 에러 안나오게 수정

--- src/main/java/com/project/traveldiary/TravelDiaryApplication.java
+++ src/main/java/com/project/traveldiary/TravelDiaryApplication.java
@@ -3,7 +3,12 @@ package com.project.traveldiary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+        org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+    }
+)
 public class TravelDiaryApplication {
 
     public static void main(String[] args) {
